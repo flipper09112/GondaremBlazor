c591d2d92a6b7b4f53268ccbdf1222362b29435c
DESKTOP-OS630H6\flipper09112 <flipper09@live.com.pt>
2024-04-12 11:44:11 +0100

    Add project files.

---
+++ GondaremBlazor.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GondaremBlazor.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ GondaremBlazor.Mobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GondaremBlazor.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
